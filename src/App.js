50580797af6577c4ef02768a6304fcc62cf863f3
Kee Tee <tkeeching@gmail.com>
2020-07-27 16:02:38 +1000

    Test PR

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
 import Navbar from './components/Navbar/Navbar'
 import HomeLeft from './components/HomeLeft/HomeLeft'
 
 function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('summary');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  })
+
+  const getData = async () => {
+    const url = `https://api.covid19api.com/${query}`
+
+    let res = await axios.get(url);
+    let data = res.data.Countries;
+    setData(data);
+    setIsLoading(false);
+
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
       <HomeLeft />
+      <div>
+        {isLoading 
+          ? <h3>loading data...</h3> 
+          : data.map((entry, index) => (
+            <div>
+              <h3 key={index}>{entry.Country}</h3>
+              <p>Total Cases: {entry.TotalConfirmed}</p>
+            </div>
+        ))}
+      </div>
     </div>
   );
 }
