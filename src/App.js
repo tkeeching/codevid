0af392ddbf316350ca61538aecfde4381c8e7b53
Kee Tee <tkeeching@gmail.com>
2020-07-29 17:11:27 +1000

    Display API data

--- src/App.js
+++ src/App.js
@@ -7,37 +7,52 @@ import HomeLeft from './components/HomeLeft/HomeLeft'
 
 function App() {
   const [data, setData] = useState([]);
-  const [query, setQuery] = useState('summary');
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getData();
-  })
+  const TOTAL_POPULATION = 25523610; // source: https://www.worldometers.info/world-population/australia-population/ 20200729
+  const states = [
+    'Total',
+    'ACT',
+    'NSW',
+    'NT',
+    'QLD',
+    'SA',
+    'TAS'
+  ]
 
   const getData = async () => {
-    const url = `https://api.covid19api.com/${query}`
-
+    const url = 'https://services1.arcgis.com/vHnIGBHHqDR6y0CR/arcgis/rest/services/COVID19_Time_Series/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json';
+    
     let res = await axios.get(url);
-    let data = res.data.Countries;
+    let data = res.data.features;
     setData(data);
     setIsLoading(false);
-
-    console.log(data);
   }
+  
+  useEffect(() => {
+    getData();
+  }, [])
 
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <HomeLeft />
-      <div>
+      <div className="api-data">
         {isLoading 
           ? <h3>loading data...</h3> 
-          : data.map((entry, index) => (
-            <div>
-              <h3 key={index}>{entry.Country}</h3>
-              <p>Total Cases: {entry.TotalConfirmed}</p>
-            </div>
-        ))}
+          : data
+              .sort((a,b) => b.attributes.Date - a.attributes.Date)
+              .map((entry, index) => (
+              <div key={index}>
+                <h3>{(new Date(entry.attributes.Date)).toString()}</h3>
+                <p>Total Cases: {entry.attributes.Total_Cases}</p>
+                <p>Total Tests: {entry.attributes.Total_Tests}</p>
+                <p>Positive Tests (by test): {((entry.attributes.Total_Cases)*100/entry.attributes.Total_Tests).toFixed(2)}%</p>
+                <p>Positive Tests (by population): {((entry.attributes.Total_Cases)*100/TOTAL_POPULATION).toFixed(2)}%</p>
+                <p>Total Deaths: {entry.attributes.Total_Deaths}</p>
+              </div>
+              ))
+          }
       </div>
     </div>
   );
