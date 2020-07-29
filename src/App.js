72009665b4327a49ab2cc74a64a5e4df66d271e5
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-07-29 10:49:46 +1000

    create footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import HomeLeft from './components/HomeLeft/HomeLeft'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HomeLeft />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer(){
+    return(
+        <div className="footer-container">
+            <h5>CODEVID23 AUSTRALIA</h5>
+            <p>Built by Hayley and Kee</p>
+            <div className="sns-icons">
+                <img src="github.png" alt="github icon"/>
+                <img src="db.png" alt="db icon"/>
+            </div>
+        </div>
+    )
+}
+
+
+export default Footer
